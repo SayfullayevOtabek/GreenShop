10f23af8a0fedaf92ebd07d89466a25e387f7d71
SayfullayevOtabek <143277839+SayfullayevOtabek@users.noreply.github.com>
2024-06-21 15:51:48 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env:{
+    PATH_URL:'http://3.70.236.23:7777/v1'
+  },
+  images:{
+    domains:[`3.70.236.23`]
+  }
+};
+
+export default nextConfig;
